3f9ac0617ca41edbf6d5da92f4ef63edbb366800
lizamihalevich <40152306+lizamihalevich@users.noreply.github.com>
2019-01-14 18:01:32 +0300

    Add files via upload

---
+++ precache-manifest.c27cb123ac51076e0faf25994342bcfa.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "535f021899c086ae32b77a3ff8ed907b",
+    "url": "/static/media/_5.535f0218.jpg"
+  },
+  {
+    "revision": "5c2592f22fc49913dac4",
+    "url": "/static/css/main.7d098bde.chunk.css"
+  },
+  {
+    "revision": "030f476e9fb1b7007ac8",
+    "url": "/static/js/1.030f476e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fae9b3088375f54cdad5978ea995b384",
+    "url": "/static/media/back.fae9b308.jpg"
+  },
+  {
+    "revision": "82f2e478b8174abf166a094fcbc8d9ba",
+    "url": "/static/media/_1.82f2e478.jpg"
+  },
+  {
+    "revision": "b7dbd6dc9a40b3c64931f41882d372c9",
+    "url": "/static/media/_2.b7dbd6dc.jpg"
+  },
+  {
+    "revision": "c7a0827faa5af709374c14d9b5217ee7",
+    "url": "/static/media/_3.c7a0827f.jpg"
+  },
+  {
+    "revision": "0729b43e4a0fa39e06ab08ac9708a66f",
+    "url": "/static/media/_4.0729b43e.jpg"
+  },
+  {
+    "revision": "5c2592f22fc49913dac4",
+    "url": "/static/js/main.5c2592f2.chunk.js"
+  },
+  {
+    "revision": "012edf777a6d12a0bac4312dec21d900",
+    "url": "/static/media/_6.012edf77.jpg"
+  },
+  {
+    "revision": "de05e1be9e2387e41bd618060f4e751f",
+    "url": "/static/media/_7.de05e1be.jpg"
+  },
+  {
+    "revision": "2c7af5bea51e47ce6af3b3b562b2522e",
+    "url": "/static/media/_8.2c7af5be.jpg"
+  },
+  {
+    "revision": "91a9c1de522e0519d6c3d8523bb82adc",
+    "url": "/static/media/_9.91a9c1de.jpg"
+  },
+  {
+    "revision": "fdd77bdff797f29c0bb61b5d83d9885e",
+    "url": "/static/media/_10.fdd77bdf.jpg"
+  },
+  {
+    "revision": "4e1c4bd3dd581759fc25d76d17158ca3",
+    "url": "/static/media/_11.4e1c4bd3.jpg"
+  },
+  {
+    "revision": "b73082ad23db8fc3be95af25f0c7e6b9",
+    "url": "/static/media/_12.b73082ad.jpg"
+  },
+  {
+    "revision": "7238777e932f2ee759d687ccf7892d37",
+    "url": "/static/media/victory.7238777e.png"
+  },
+  {
+    "revision": "c39cb3bbfae1ca0a2f32e9363e769927",
+    "url": "/index.html"
+  }
+];
